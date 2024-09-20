a91eacf87c87fa578ea1d3d05fd35569543e7313
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-09-20 09:48:32 -0300

    aps

---
+++ src/Customer.ts
@@ -0,0 +1,28 @@
+
+//customer.ts
+import Address from "./Address";
+
+export default class Customer {
+    name: string;
+    age: number;
+    email: string;
+    birthDate: Date;
+    address: Address;
+
+    constructor(name: string, email: string, birthDate: Date, address: Address) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.age = new Date().getFullYear() - birthDate.getFullYear();
+        this.address = address
+    }
+
+    isAdult() {
+        return this.age >= 18;
+    }
+
+    getFirstName() {
+        return this.name.split(" ")[0];
+    }
+    
+}
---
+++ src/Display.ts
@@ -0,0 +1,7 @@
+
+export default class Display {
+    showNumber(value: number) {
+        return value;
+        
+    }
+}
