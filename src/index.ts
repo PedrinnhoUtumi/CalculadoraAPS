696e1269543184adff55f308fac2927590bb412b
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-09-27 09:55:46 -0300

    implementação do código do prof

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,3 @@
 // tela.showNumber(9)
 
 // minhaTela.showNumber(213)
-
-import { Cpu } from "./Cpu";
-const d = new Cpu()
-d.Adicao(1, 3)
-d.RaizQuadrada(4)
-try {
-    d.Divisao(2, 0)
-} catch(error) {
-    console.error(error)
-}
-d.Exponenciacao(9, 0)
